c2782adb4786a3e1fd35e7a2a467faa5a4d26f08
Makc Berluskone <makc.berluskone@gmail.com>
2024-09-04 20:39:08 +0800

    feat: base setting admin panel layout and add OrderResource

---
+++ app/MoonShine/MoonShineLayout.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine;
+
+use MoonShine\Components\Layout\{Content,
+    Flash,
+    Footer,
+    Header,
+    LayoutBlock,
+    LayoutBuilder,
+    Menu,
+    Profile,
+    Search,
+    Sidebar};
+use MoonShine\Components\When;
+use MoonShine\Contracts\MoonShineLayoutContract;
+
+final class MoonShineLayout implements MoonShineLayoutContract
+{
+    public static function build(): LayoutBuilder
+    {
+        return LayoutBuilder::make([
+            Sidebar::make([
+                Menu::make(),
+                When::make(
+                    static fn() => config('moonshine.auth.enable', true),
+                    static fn() => [Profile::make(withBorder: true)]
+                ),
+            ]),
+            LayoutBlock::make([
+                Flash::make(),
+                Header::make([
+                    Search::make(),
+                ]),
+                Content::make(),
+                Footer::make()
+                    ->copyright(fn(): string => sprintf(
+                        <<<'HTML'
+                            &copy; %d
+                        HTML,
+                        now()->year
+                    ))
+                    ->menu([
+//                        'https://moonshine-laravel.com/docs' => 'Documentation',
+                    ]),
+            ])->customAttributes(['class' => 'layout-page']),
+        ]);
+    }
+}
---
+++ app/MoonShine/Resources/OrderResource.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Domain\Order\Models\Order;
+use Illuminate\Database\Eloquent\Model;
+use MoonShine\Components\MoonShineComponent;
+use MoonShine\Decorations\Block;
+use MoonShine\Fields\Field;
+use MoonShine\Fields\ID;
+use MoonShine\Resources\ModelResource;
+
+/**
+ * @extends ModelResource<Order>
+ */
+class OrderResource extends ModelResource
+{
+    protected string $model = Order::class;
+
+    protected string $title = 'Заказы';
+
+    /**
+     * @return list<MoonShineComponent|Field>
+     */
+    public function fields(): array
+    {
+        return [
+            Block::make([
+                ID::make()->sortable(),
+            ]),
+        ];
+    }
+
+    /**
+     * @param Order $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    public function rules(Model $item): array
+    {
+        return [];
+    }
+}
